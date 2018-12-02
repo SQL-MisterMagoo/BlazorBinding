6e69a84691b36e5a2027c79acad721975fd2696b
Howard Richards <dev@conficient.com>
2018-12-02 15:07:04 +0000

    Added Code Syntax Highlighting capability to the DisplayCode component.

---
+++ BlazorBinding/wwwroot/js/displayCode.js
@@ -0,0 +1,13 @@
+﻿// get element inner text
+function getInnerText(id) {
+    console.log("trying to locate " + id);
+    var e = document.getElementById(id);
+    return e.innerText;
+}
+
+// set element HTML
+function setHtml(id, text) {
+    console.log("setting the inner text on " + id);
+    var e = document.getElementById(id);
+    e.innerHTML = text;
+}
