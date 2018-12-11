6d812e6f28026d60480c3652e72e65e925473f96
Mister Magoo <mister.magoo+githubsql@gmail.com>
2018-12-11 23:55:57 +0000

    Add helper js for setting style properties

---
+++ BlazorBinding/wwwroot/js/Helpers.js
@@ -0,0 +1,11 @@
+﻿window.helpers = {
+    setProperty: function (el, prop, value) {
+        try {
+            document.querySelector(el).style.setProperty(prop, value);
+            return true;
+        } catch (e) {
+            console.log(e);
+            return false;
+        }
+    }
+};
