3d459f87adb89be26e6624a77453c735fc35cb7a
Mister Magoo <mister.magoo+githubsql@gmail.com>
2018-12-12 23:34:39 +0000

    Moved changelog to be data-driven from changelog.json Added Readme page that uses markdig to display the README.md file

---
+++ BlazorBinding/Components/DocumentationSupport/ChangeLogItems.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBinding.Components.DocumentationSupport
+{
+	public class ChangeLog
+	{
+		public DateTime dated { get; set; }
+		public string content { get; set; }
+		public Child[] child { get; set; }
+	}
+
+	public class Child
+	{
+		public string text { get; set; }
+	}
+
+}
+
