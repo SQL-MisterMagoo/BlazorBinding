8f33eef4574d8996c83370d7ff26851665b48895
Mister Magoo <mister.magoo+githubsql@gmail.com>
2019-01-05 23:32:43 +0000

    Added BlazorComponentExt as the default component base class - this enables the use of the new ModelAttribute, which auto hooks up StateHasChanged() to IPropertyNotifyChanged events. Moved all @using statements to the base _ViewImports.cshtml to make it easier to add new pages.

---
+++ BlazorBinding/Components/BlazorComponentExt.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using System;
+using System.ComponentModel;
+using System.Linq;
+using Blazor = Microsoft.AspNetCore.Blazor.Components;
+
+namespace BlazorBinding.Components
+{
+	public partial class BlazorComponentExt : Blazor.BlazorComponent
+	{
+		[Blazor.Parameter] protected RenderFragment Body { get; set; }
+
+		protected override void OnInit()
+		{
+			Console.WriteLine("MYINIT");
+			base.OnInit();
+			RegisterModels(GetType());
+		}
+
+		private void RegisterModels(Type type)
+		{
+			Console.WriteLine("REGISTER");
+			var props = ((System.Reflection.TypeInfo)type).DeclaredProperties;
+			foreach (var prop in props)
+			{
+				var attr = (ModelAttribute)Attribute.GetCustomAttribute(prop, typeof(ModelAttribute));
+				if (attr is null)
+				{
+					Console.WriteLine($"Property {prop.Name} is not a Model");
+				}
+				else
+				{
+					Console.WriteLine($"Property {prop.Name} is a model called {prop.PropertyType.Name}");
+					var model = prop.GetValue(this);
+					if (model is null)
+					{
+						Console.WriteLine("The model is null, newing up...");
+						model = Activator.CreateInstance(prop.PropertyType);
+						prop.SetValue(this, model);
+					}
+					((INotifyPropertyChanged)model).PropertyChanged += Vm_PropertyChanged;
+				}
+			}
+		}
+
+		private void Vm_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			Console.WriteLine($"Property {e.PropertyName} changed on {sender.GetType().Name}");
+			StateHasChanged();
+		}
+	}
+
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+	sealed class ModelAttribute : Attribute
+	{
+		public ModelAttribute() { }
+	}
+}
---
+++ BlazorBinding/Models/DataModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BlazorBinding.Models
+{
+	class DataModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+		bool _toggle;
+
+		public bool Toggle
+		{
+			get => _toggle;
+			set
+			{
+				if (value != _toggle)
+				{
+					_toggle = value;
+					RaisePropertyChanged();
+				}
+			}
+		}
+
+		private void RaisePropertyChanged([CallerMemberName] string name = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+	}
+}
